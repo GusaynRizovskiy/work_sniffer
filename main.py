803eecefbcdc1bf41c0c79384499c21b0e1b81b3
Maks <makkks200426013@mail.ru>
2025-01-30 16:32:07 +0300

    Добавил небольшие изменения

--- main.py
+++ main.py
@@ -321,10 +321,18 @@ class Form_main(QtWidgets.QMainWindow,Form1):
         self.close()
 
 if __name__ == '__main__':
+    if __name__ == '__main__':
+        if getattr(sys, 'frozen', False):
+            base_path = sys._MEIPASS  # Путь к временной директории при запуске из EXE
+        else:
+            base_path = os.path.dirname(__file__)
+
+        image_path = os.path.join(base_path, 'fon', 'picture_fon.jpg')
+
     app = QtWidgets.QApplication(sys.argv)
     form = Form_main()
     palette = QPalette()
-    palette.setBrush(QPalette.Background, QBrush(QPixmap("picture_fon.jpg")))
+    palette.setBrush(QPalette.Background, QBrush(QPixmap(image_path)))
     form.setPalette(palette)
     form.show()
     sys.exit(app.exec_())
